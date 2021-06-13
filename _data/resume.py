6ed41b1b563cdbf8b5119be33885eb656b28bcd0
urbanjoe22 <joegxu22@gmail.com>
2021-06-13 02:42:48 -0400

    change the intro

--- _data/resume.py
+++ _data/resume.py
@@ -5,6 +5,6 @@ COUNTRY_LOCATION = "USA"
 PHONE_NUMBER = "+1-480-570-5798"
 EMAIL_ADDRESS = "joegxu22@gmail.com"
 
-PERSONAL_STATEMENT = "Hello, my name is 徐冠舟, but you can call me Joe! A Computer Science professional who studied @ The University of Alabama; currently shaping my professional career; inquisitive, laidback, and also driven. Thoughtful of the past, feeling in the moment, and foolishly optimistic about the future."
+PERSONAL_STATEMENT = "Hello, my name is 徐冠舟, but you can call me Joe! A Lifelong Explorer who good for a story or two; currently shaping my professional career; inquisitive, laidback, and also driven. Thoughtful of the past, feeling in the moment, and foolishly optimistic about the future."
 TAGLINE = "*Heart of a Rebel, Soul of a Tool - (╯°□°）╯︵ ┻━┻ | ┬──┬◡ﾉ(° -°ﾉ)"
 ABOUT = "Born in the Far East (China), raised in the Southwest (Phoenix), studied in the deep South (Alabama). A philosopher by heart, an engineer by trade. Adding value to the world one line of code at a time."
